05e1e1233e64b324584476d401f3d6171f6aab7a
Anthony Jones <anthony@vandacoatings.co.uk>
2023-03-14 17:16:46 +0000

    Test table on db

--- app/models.py
+++ app/models.py
@@ -24,3 +24,6 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+class Test(db.Model):
+    test=db.Column(db.String(128), index = True)
