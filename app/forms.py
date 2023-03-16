8b1ca63734d187b01da9c2706ee1cc2ae5a5e81f
Richard Johns <johnsr12@cardiff.ac.uk>
2023-03-16 18:04:03 +0000

    select questions from dropdown, create formativetest database and see them

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,36 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
+from wtforms_sqlalchemy.fields import QuerySelectField
+from app.models import Multiplechoice
+
 
 class LoginForm(FlaskForm):
     username = StringField('Your ID number', validators = [DataRequired()])
     password = PasswordField('Password', validators = [DataRequired()])
     submit = SubmitField('Sign in')
+
+class QuestionForm(FlaskForm):
+    question=StringField(validators=[DataRequired(message="Question is required")])
+    answer1=StringField (validators=[DataRequired(message="Minimum of two answer choices are required")])
+    ans_multi_select_1 = BooleanField("Option 1")
+    answer2=StringField(validators=[DataRequired(message="Minimum of two answer choices are required")])
+    ans_multi_select_2 = BooleanField("Option 2")
+    answer3=StringField(FlaskForm)
+    ans_multi_select_3 = BooleanField("Option 3")
+    answer4=StringField(FlaskForm)
+    ans_multi_select_4 = BooleanField("Option 4")
+
+    marks=IntegerField(validators=[DataRequired(message="Please enter mark")]) 
+    feedback=StringField("Feedback:")
+    submit = SubmitField("Form Complete")
+
+def MC_query():
+    return Multiplechoice.query
+
+class MCChoiceForm(FlaskForm):
+    question_1 = QuerySelectField(query_factory=MC_query, allow_blank=True, get_label='question')
+    question_2 = QuerySelectField(query_factory=MC_query, allow_blank=True, get_label='question')
+    question_3 = QuerySelectField(query_factory=MC_query, allow_blank=True, get_label='question')
+    question_4 = QuerySelectField(query_factory=MC_query, allow_blank=True, get_label='question')
+    question_5 = QuerySelectField(query_factory=MC_query, allow_blank=True, get_label='question')
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, g,session
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
-from app.models import User
-from app.forms import LoginForm
-from app import app
+from app.models import User, Multiplechoice, FormativeTest
+from app.forms import LoginForm, QuestionForm, MCChoiceForm
+from app import app, db
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -11,6 +11,7 @@ def login():
     form = LoginForm()
 
     if current_user.is_authenticated:
+        
         return redirect(url_for('index'))
 
     if form.validate_on_submit():
@@ -42,3 +43,65 @@ def logout():
 def index():
     return render_template('index.html', title = 'Home')
 
+# Add multiple choice questions
+@app.route('/mc_questions', methods=['GET', 'POST'])
+@login_required
+def question():
+    form=QuestionForm()
+    
+    if form.validate_on_submit():
+        multi= Multiplechoice(
+        user_id=current_user.id,
+        question=form.question.data, 
+        answer_1=form.answer1.data,ans_choice_1=form.ans_multi_select_1.data, 
+        answer_2=form.answer2.data,ans_choice_2=form.ans_multi_select_2.data, 
+        answer_3=form.answer3.data, ans_choice_3=form.ans_multi_select_3.data,
+        answer_4=form.answer4.data, ans_choice_4=form.ans_multi_select_4.data,
+        marks=form.marks.data, 
+        feedback=form.feedback.data
+        )
+        db.session.add(multi)
+        db.session.commit()
+        flash('Your question is added!')
+        return redirect('/mc_questions')
+    return render_template("mc_questions.html", title="Add Multiple Choice Questions", form=form)
+
+@app.route("/question_list",methods=['GET'])
+def result():
+    
+    questions=Multiplechoice.query.all()
+    
+        
+    return render_template('question_list.html',questions=questions)
+
+@app.route('/create_form_test', methods=['GET', 'POST'])
+@login_required
+def create_form_test():
+    form = MCChoiceForm()
+    if form.validate_on_submit():
+        formtest= FormativeTest(
+        author=current_user.id,
+        mcquestion_1_id=form.question_1.data.id,
+        mcquestion_1=form.question_1.data.question,
+        mcquestion_2_id=form.question_2.data.id,
+        mcquestion_2=form.question_2.data.question,
+        mcquestion_3_id=form.question_3.data.id,
+        mcquestion_3=form.question_3.data.question,
+        mcquestion_4_id=form.question_4.data.id,
+        mcquestion_4=form.question_4.data.question,
+        mcquestion_5_id=form.question_5.data.id,
+        mcquestion_5=form.question_5.data.question,
+        )
+        db.session.add(formtest)
+        db.session.commit()
+        flash('Your test is added!')
+        return redirect('/create_form_test')
+    return render_template('create_form_test.html', title = 'Create Formative Assesment', form=form)
+
+@app.route("/Formative_test_list",methods=['GET'])
+def formtests():
+    
+    formtests=FormativeTest.query.all()
+    
+        
+    return render_template('Formative_test_list.html',formtests=formtests)
