a1e3c0d62022acce74c5538a2a0cbe5d244379c1
Emma Wilson <wilsone8@cardiff.ac.uk>
2023-04-12 16:44:59 +0000

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1250,3 +1250,12 @@ def release_formtest(Form_test_id):
         test.available_to_take = True
     db.session.commit()
     return redirect('/Formative_test_list')
+
+#EMMA
+@app.route("/your_results", methods=['GET'])
+@login_required
+def your_results():
+
+    user_id = session.get('user_id')
+    individ_results=Results_sum.query.filter_by(user_id=user_id).all()
+    return render_template('your_results.html', title = 'Your Results', individ_results = individ_results)
